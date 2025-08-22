34a66436b2a7bdd1f220794902836ae76e67eb21
Martin Torres <tmartineduardo@gmail.com>
2025-08-22 17:42:39 -0300

    Initial setup

---
+++ app/(home)/index.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export default function HomeScreen() {
+    return(
+        <Text className="bg-black text-white text-lg pt-10">
+            ModakChallenge
+        </Text>
+    ) 
+}
