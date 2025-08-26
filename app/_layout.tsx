9d3f2d4b7693a0350f02ca15732bbdcacfb9c99f
Martin Torres <tmartineduardo@gmail.com>
2025-08-26 12:25:41 -0300

    Handle font loaded in return to avoid hooks order error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import 'react-native-reanimated';
 import "../global.css";
 
@@ -13,40 +14,42 @@ export default function RootLayout() {
   });
   const [queryClient] = useState(() => new QueryClient());
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   const headerStyle = { backgroundColor: themeColors.secondaryBg };
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen 
-          name="(home)/index" 
-          options={{ 
-            headerShown: true, 
-            title: 'Modak challenge',
-            headerStyle: headerStyle
-          }} 
-        />
-        <Stack.Screen 
-          name="product/[id]" 
-          options={{
-            title: 'Product',
-            headerStyle: headerStyle
-          }} 
-        />
-        <Stack.Screen 
-          name="category/[slug]" 
-          options={{ 
-            headerShown: true, 
-            title: 'Modak challenge',
-            headerStyle: headerStyle
-          }} 
-        />
-      </Stack>
+      {!loaded ?
+        <View>
+          <ActivityIndicator size='large' />
+        </View>
+        : (
+        <Stack>
+          <Stack.Screen 
+            name="(home)/index" 
+            options={{ 
+              headerShown: true, 
+              title: 'Modak challenge',
+              headerStyle: headerStyle
+            }} 
+          />
+          <Stack.Screen 
+            name="product/[id]" 
+            options={{
+              title: 'Product',
+              headerStyle: headerStyle
+            }} 
+          />
+          <Stack.Screen 
+            name="category/[slug]" 
+            options={{ 
+              headerShown: true, 
+              title: 'Modak challenge',
+              headerStyle: headerStyle
+            }} 
+          />
+        </Stack>
+        )
+      } 
       <StatusBar style="auto" />
     </QueryClientProvider>
   );
